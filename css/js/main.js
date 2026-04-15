46924e396d8e07bec282e2ddb6a42867a4ccc528
Marriamer <mgalindocastillo6@gmail.com>
2026-04-15 12:25:11 -0500

    update portfolio with animations and styles

--- css/js/main.js
+++ css/js/main.js
@@ -1,31 +1,17 @@
-let ubicacionPrincipal = window.pageYOffset; //0
+// Animación al hacer scroll
+const elements = document.querySelectorAll('.fade-in');
 
-  AOS.init();
+const showOnScroll = () => {
+  elements.forEach(el => {
+    const top = el.getBoundingClientRect().top;
 
-window.addEventListener("scroll", function(){
-    let desplazamientoActual = window.pageYOffset; //180
-    if(ubicacionPrincipal >= desplazamientoActual){ // 200 > 180
-        document.getElementsByTagName("nav")[0].style.top = "0px"
-    }else{
-        document.getElementsByTagName("nav")[0].style.top = "-100px"
+    if (top < window.innerHeight - 100) {
+      el.classList.add('visible');
     }
-    ubicacionPrincipal= desplazamientoActual; //200
+  });
+};
 
-})
+window.addEventListener('scroll', showOnScroll);
 
-// Menu
-
-let enlacesHeader = document.querySelectorAll(".enlaces-header")[0];
-let semaforo = true;
-
-document.querySelectorAll(".hamburguer")[0].addEventListener("click", function(){
-    if(semaforo){
-        document.querySelectorAll(".hamburguer")[0].style.color ="#fff";
-        semaforo= false;
-    }else{
-        document.querySelectorAll(".hamburguer")[0].style.color ="#000";
-        semaforo= true;
-    }
-
-    enlacesHeader.classList.toggle("menudos")
-})
+// Ejecutar al cargar
+showOnScroll();
